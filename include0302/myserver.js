2a8fafbd7ecefeda76592a4c732771fdbb3d418e
JJY <jyjang1222@naver.com>
2023-01-02 14:47:44 +0900

    노드 include 학습내용 추가

---
+++ include0302/myserver.js
@@ -0,0 +1,32 @@
+// 서버 세팅 
+var express = require("express");
+var app = express();
+var port = 3303;
+var server = app.listen(port, function(){
+	console.log("서버가 가동되었습니다" + port);
+});
+
+// ejs 세팅(views)
+var ejs = require("ejs");
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.engine("ejs", ejs.renderFile);
+
+// img폴더 경로 셋팅
+app.use(express.static(__dirname + '/img'));
+
+// session 세팅
+var session = require("express-session");
+app.use(session({							// session에는 옵션3가지를 설정해야줘야 한다. 
+	secret : "abcdefg",						// secret에는 아무 내용이나 작성해주고,
+	resave : false,							// 나머지는 false로 설정한다.
+	saveUninitialized : false				// 나머지는 false로 설정한다.
+}));
+
+// 라우터 세팅
+app.get('/', (req, res) => {
+    res.redirect('main');
+});
+app.get('/main', (req, res) => {
+    res.render('main.ejs');
+});
